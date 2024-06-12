6db8f341dcc30abe807da79f34c25b9a7521bcb8
joey.wang <joey.wang@cloudminds.com>
2024-06-12 11:10:13 +0800

    finetune kespeech - 15th

--- examples/industrial_data_pretraining/contextual_paraformer/finetune_kespeech_A100.sh
+++ examples/industrial_data_pretraining/contextual_paraformer/finetune_kespeech_A100.sh
@@ -70,9 +70,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   --nproc_per_node ${gpu_num} \
   ../../../funasr/bin/train.py \
   ++model="${model_name_or_model_dir}" \
-  ++model_conf="${model_name_or_model_dir}/config.yaml"
   ++model_revision="${model_revision}" \
-  ++init_param="${model_name_or_model_dir}/model.pt.ep5" \
   ++train_data_set_list="${train_data}" \
   ++valid_data_set_list="${val_data}" \
   ++dataset_conf.batch_size=${batch_size} \
@@ -80,7 +78,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   ++dataset_conf.num_workers=${num_workers} \
   ++train_conf.max_epoch=${max_epoch} \
   ++train_conf.log_interval=1 \
-  ++train_conf.resume=true \
+  ++train_conf.resume=false \
   ++train_conf.validate_interval=2000 \
   ++train_conf.save_checkpoint_interval=2000 \
   ++train_conf.keep_nbest_models=${keep_nbest_models} \
