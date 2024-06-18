99495af4b1e75954f543f0d9af7a497af8e842aa
joey.wang <joey.wang@cloudminds.com>
2024-06-18 10:35:31 +0800

    finetune kespeech - 31h

--- examples/industrial_data_pretraining/contextual_paraformer/finetune_kespeech_task_1.sh
+++ examples/industrial_data_pretraining/contextual_paraformer/finetune_kespeech_task_1.sh
@@ -4,19 +4,19 @@
 # method1, finetune from model hub
 
 
-__conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
-        . "/opt/conda/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/conda/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
-conda activate /datas/runtimes/env_funasr
+#__conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+#if [ $? -eq 0 ]; then
+#    eval "$__conda_setup"
+#else
+#    if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
+#        . "/opt/conda/etc/profile.d/conda.sh"
+#    else
+#        export PATH="/opt/conda/bin:$PATH"
+#    fi
+#fi
+#unset __conda_setup
+#
+#conda activate /datas/runtimes/env_funasr
 
 # which gpu to train or finetune
 export CUDA_VISIBLE_DEVICES="0"
