69637b19783e9194c7333945ae625f5c9c48e8bc
WangCK0809 <2024685259@qq.com>
2024-06-18 02:28:58 +0000

    finetune kespeech - 30h

--- examples/industrial_data_pretraining/contextual_paraformer/finetune_kespeech_task_1.sh
+++ examples/industrial_data_pretraining/contextual_paraformer/finetune_kespeech_task_1.sh
@@ -71,7 +71,7 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
 fi
 
 # exp output dir
-output_dir="./outputs_A100_2"
+output_dir="${root}/outputs"
 log_file="${output_dir}/log.txt"
 
 
@@ -98,7 +98,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   ++dataset_conf.num_workers=${num_workers} \
   ++train_conf.max_epoch=${max_epoch} \
   ++train_conf.log_interval=1 \
-  ++train_conf.resume=true \
+  ++train_conf.resume=false \
   ++train_conf.validate_interval=2000 \
   ++train_conf.save_checkpoint_interval=2000 \
   ++train_conf.keep_nbest_models=${keep_nbest_models} \
